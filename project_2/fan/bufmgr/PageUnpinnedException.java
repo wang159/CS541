6af8ef1b5e6a38738b072e9c16c78c39263769ca
Fan Chen <chen926@lore.cs.purdue.edu>
2015-03-08 16:27:24 -0400

    add files by fan

---
+++ project_2/fan/bufmgr/PageUnpinnedException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bufmgr;
+
+/**
+ *
+ * @author wei
+ */
+public class PageUnpinnedException extends Exception {
+    private String page_is_not_pined_yet;
+
+    public PageUnpinnedException(Object object, String page_is_not_pined_yet) {
+    }
+
+    
+    
+}
